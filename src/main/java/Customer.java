01e75b2883ecfd5bdfd89c1fce14cc52565a0901
Zulema Campos <zulemacampos@Zulemas-Air.fios-router.home>
2022-02-24 14:23:48 -0500

    added customer class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,8 +1,19 @@
 import staff.Person;
 
 public class Customer<P> extends Person {
-    git
+    private Integer idCount = 0;
+    private Integer idNumber;
+   // private P item;
+
     public Customer(String name) {
         super(name);
     }
+    public String greeting(){
+        return "Hello, I'm fine thank you for asking";
+    }
+
+    public String makePurchase(P item){
+        return "You purchased " + item;
+    }
+
 }
